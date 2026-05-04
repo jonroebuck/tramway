7e112e7b43f7bd29d90e2cb19a1bcb4ee7c99e00
jonroebuck <jon_roebuck@hotmail.com>
2026-05-04 00:42:32 -0400

    Initialize examples module with imports

---
+++ clients/tramway-py/tramway/examples/__init__.py
@@ -0,0 +1,5 @@
+from .client import Tramway
+from .builder import CompletionBuilder, TramwayExtensions
+from .exceptions import TramwayException
+
+__all__ = ["Tramway", "CompletionBuilder", "TramwayExtensions", "TramwayException"]
